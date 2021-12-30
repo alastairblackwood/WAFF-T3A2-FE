a43ef2b3325e4dee4523fb0b2d29a0bdea50244e
NataliaCK <natalia.chamom@gmail.com>
2021-12-30 18:50:56 +1000

    added more code for dashboard

--- src/Components/adminDashboard/FilmList/FilmList.js
+++ src/Components/adminDashboard/FilmList/FilmList.js
@@ -1,39 +1,443 @@
-import React from 'react';
-import styled from 'styled-components';
-
+import React from "react";
+import styled from "styled-components";
 
 const FilmList = () => {
-    return (
-        <div>
-            <FilmListContainer className='Container'>
-                <FilmEditContainer>
+  return (
+    <div>
+      <FilmListContainer>
+        <Product>
+          <ProductTitleContainer>
+            <ProductTitle>FILM LIST</ProductTitle>
+            <Link to="#" />
+            <ProductAddButton>Create</ProductAddButton>
+          </ProductTitleContainer>
+          <ProductTop>
+            <ProductTopLeft>
+              <Chart
+                data={ProductData}
+                dataKey="Sales"
+                title="Sales Performance"
+              />
+            </ProductTopLeft>
+            <ProductTopRight>
+              <ProductInfoTop>
+                <ProductInfoImg src="#" alt="" />
+                <ProductName>Rushmore</ProductName>
+              </ProductInfoTop>
+              <ProductInfoBottom>
+                <ProductInfoItem>
+                  <ProductInfoKey>id:</ProductInfoKey>
+                  <ProductInfoValue>123</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>sales:</ProductInfoKey>
+                  <ProductInfoValue>5123</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>active:</ProductInfoKey>
+                  <ProductInfoValue>yes</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>in stock:</ProductInfoKey>
+                  <ProductInfoValue>no</ProductInfoValue>
+                </ProductInfoItem>
+              </ProductInfoBottom>
+            </ProductTopRight>
+          </ProductTop>
 
-                </FilmEditContainer>
-            </FilmListContainer>
-            
-        </div>
-    )
-}
+          <ProductTop>
+            <ProductTopLeft>
+              <Chart
+                data={ProductData}
+                dataKey="Sales"
+                title="Sales Performance"
+              />
+            </ProductTopLeft>
+            <ProductTopRight>
+              <ProductInfoTop>
+                <ProductInfoImg src="#" alt="" />
+                <ProductName>The Royal Tenenbaums</ProductName>
+              </ProductInfoTop>
+              <ProductInfoBottom>
+                <ProductInfoItem>
+                  <ProductInfoKey>id:</ProductInfoKey>
+                  <ProductInfoValue>123</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>sales:</ProductInfoKey>
+                  <ProductInfoValue>5123</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>active:</ProductInfoKey>
+                  <ProductInfoValue>yes</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>in stock:</ProductInfoKey>
+                  <ProductInfoValue>no</ProductInfoValue>
+                </ProductInfoItem>
+              </ProductInfoBottom>
+            </ProductTopRight>
+          </ProductTop>
 
-export default FilmList
+          <ProductTop>
+            <ProductTopLeft>
+              <Chart
+                data={ProductData}
+                dataKey="Sales"
+                title="Sales Performance"
+              />
+            </ProductTopLeft>
+            <ProductTopRight>
+              <ProductInfoTop>
+                <ProductInfoImg src="#" alt="" />
+                <ProductName>The Life Aquatic with Steve Zissou</ProductName>
+              </ProductInfoTop>
+              <ProductInfoBottom>
+                <ProductInfoItem>
+                  <ProductInfoKey>id:</ProductInfoKey>
+                  <ProductInfoValue>123</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>sales:</ProductInfoKey>
+                  <ProductInfoValue>5123</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>active:</ProductInfoKey>
+                  <ProductInfoValue>yes</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>in stock:</ProductInfoKey>
+                  <ProductInfoValue>no</ProductInfoValue>
+                </ProductInfoItem>
+              </ProductInfoBottom>
+            </ProductTopRight>
+          </ProductTop>
+
+          <ProductTop>
+            <ProductTopLeft>
+              <Chart
+                data={ProductData}
+                dataKey="Sales"
+                title="Sales Performance"
+              />
+            </ProductTopLeft>
+            <ProductTopRight>
+              <ProductInfoTop>
+                <ProductInfoImg src="#" alt="" />
+                <ProductName>The Darjeeling Limited</ProductName>
+              </ProductInfoTop>
+              <ProductInfoBottom>
+                <ProductInfoItem>
+                  <ProductInfoKey>id:</ProductInfoKey>
+                  <ProductInfoValue>123</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>sales:</ProductInfoKey>
+                  <ProductInfoValue>5123</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>active:</ProductInfoKey>
+                  <ProductInfoValue>yes</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>in stock:</ProductInfoKey>
+                  <ProductInfoValue>no</ProductInfoValue>
+                </ProductInfoItem>
+              </ProductInfoBottom>
+            </ProductTopRight>
+          </ProductTop>
+
+          <ProductTop>
+            <ProductTopLeft>
+              <Chart
+                data={ProductData}
+                dataKey="Sales"
+                title="Sales Performance"
+              />
+            </ProductTopLeft>
+            <ProductTopRight>
+              <ProductInfoTop>
+                <ProductInfoImg src="#" alt="" />
+                <ProductName>Fantastic Mr. Fox </ProductName>
+              </ProductInfoTop>
+              <ProductInfoBottom>
+                <ProductInfoItem>
+                  <ProductInfoKey>id:</ProductInfoKey>
+                  <ProductInfoValue>123</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>sales:</ProductInfoKey>
+                  <ProductInfoValue>5123</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>active:</ProductInfoKey>
+                  <ProductInfoValue>yes</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>in stock:</ProductInfoKey>
+                  <ProductInfoValue>no</ProductInfoValue>
+                </ProductInfoItem>
+              </ProductInfoBottom>
+            </ProductTopRight>
+          </ProductTop>
+
+          <ProductTop>
+            <ProductTopLeft>
+              <Chart
+                data={ProductData}
+                dataKey="Sales"
+                title="Sales Performance"
+              />
+            </ProductTopLeft>
+            <ProductTopRight>
+              <ProductInfoTop>
+                <ProductInfoImg src="#" alt="" />
+                <ProductName>Moonrise Kingdom</ProductName>
+              </ProductInfoTop>
+              <ProductInfoBottom>
+                <ProductInfoItem>
+                  <ProductInfoKey>id:</ProductInfoKey>
+                  <ProductInfoValue>123</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>sales:</ProductInfoKey>
+                  <ProductInfoValue>5123</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>active:</ProductInfoKey>
+                  <ProductInfoValue>yes</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>in stock:</ProductInfoKey>
+                  <ProductInfoValue>no</ProductInfoValue>
+                </ProductInfoItem>
+              </ProductInfoBottom>
+            </ProductTopRight>
+          </ProductTop>
+
+          <ProductTop>
+            <ProductTopLeft>
+              <Chart
+                data={ProductData}
+                dataKey="Sales"
+                title="Sales Performance"
+              />
+            </ProductTopLeft>
+            <ProductTopRight>
+              <ProductInfoTop>
+                <ProductInfoImg src="#" alt="" />
+                <ProductName>The Grand Budapest Hotel</ProductName>
+              </ProductInfoTop>
+              <ProductInfoBottom>
+                <ProductInfoItem>
+                  <ProductInfoKey>id:</ProductInfoKey>
+                  <ProductInfoValue>123</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>sales:</ProductInfoKey>
+                  <ProductInfoValue>5123</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>active:</ProductInfoKey>
+                  <ProductInfoValue>yes</ProductInfoValue>
+                </ProductInfoItem>
+                <ProductInfoItem>
+                  <ProductInfoKey>in stock:</ProductInfoKey>
+                  <ProductInfoValue>no</ProductInfoValue>
+                </ProductInfoItem>
+              </ProductInfoBottom>
+            </ProductTopRight>
+          </ProductTop>
+
+          <ProductBottom>
+            <ProductForm>
+              <ProductFormLeft>
+                <Label>Film Name</Label>
+                <Input type="text" placeholder="Apple AirPod" />
+                <Label>In Stock</Label>
+                <Select name="inStock" id="idStock">
+                  <Option value="yes">Yes</Option>
+                  <Option value="no">No</Option>
+                </Select>
+                <Label>Rated</Label>
+                <Select name="active" id="active">
+                  <Option value="yes">Yes</Option>
+                  <Option value="no">No</Option>
+                </Select>
+              </ProductFormLeft>
+              <ProductFormRight>
+                <ProductUpload>
+                  <ProductUploadImg src="#" alt="" />
+                  <Label for="file">
+                    <Publish />
+                  </Label>
+                  <Input type="file" id="file" style={{ display: "none" }} />
+                </ProductUpload>
+                <ProductButton>Update</ProductButton>
+              </ProductFormRight>
+            </ProductForm>
+          </ProductBottom>
+        </Product>
+      </FilmListContainer>
+    </div>
+  );
+};
+
+export default FilmList;
 
 const FilmListContainer = styled.div`
-    justify-content: space-between;
-    align-items: left;
-    flex-wrap: wrap;
-    position: absolute;    
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
 
-    display: flex;
-    width: 978px;
-    height: 970px;
-    left: 414px;
-    top: 130px;
+  position: absolute;
+  width: 1296px;
+  height: 970px;
+  left: 72px;
+  top: 146px;
 
-    background: #C4C4C4;
-    mix-blend-mode: soft-light;
-    border-radius: 83px;
+  background: #555555;
+  mix-blend-mode: soft-light;
+  border-radius: 83px;
 `;
 
-const FilmEditContainer = styled.div`
+// const FilmEditContainer = styled.div`
+//   justify-content: space-between;
+//   align-items: center;
+//   flex-wrap: wrap;
+//   position: absolute;
 
+//   width: 950px;
+//   height: 261px;
+//   left: 231px;
+//   top: 288px;
+
+//   background: #cdc9c3;
+//   border-radius: 39px;
+// `;
+
+// const FilmInfoContainer = styled.div``;
+
+const Product = styled.div`
+  flex: 4;
+  padding: 150px;
 `;
+
+const ProductTitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const ProductTitle = styled.h1``;
+
+const ProductAddButton = styled.button`
+  width: 80px;
+  border: none;
+  padding: 5px;
+  background-color: teal;
+  color: white;
+  border-radius: 5px;
+  font-size: 16px;
+  cursor: pointer;
+`;
+
+const ProductTop = styled.div`
+  display: flex;
+`;
+
+const ProductTopLeft = styled.div`
+  flex: 1;
+`;
+
+const ProductTopRight = styled.div`
+  flex: 1;
+  padding: 20px;
+  margin: 20px;
+  -webkit-box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
+  box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
+`;
+
+const ProductInfoTop = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ProductInfoImg = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  object-fit: cover;
+  margin-right: 20px;
+`;
+
+const ProductName = styled.span`
+  font-weight: 600;
+`;
+
+const ProductInfoBottom = styled.div`
+  margin-top: 10px;
+`;
+
+const ProductInfoItem = styled.div`
+  width: 150px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ProductInfoKey = styled.span``;
+
+const ProductInfoValue = styled.span`
+  font-weight: 300;
+`;
+
+const ProductBottom = styled.div`
+  padding: 20px;
+  margin: 20px;
+  -webkit-box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
+  box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
+`;
+
+const ProductForm = styled.form`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ProductFormLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Label = styled.label`
+  margin-bottom: 10px;
+  color: gray;
+`;
+
+const Input = styled.input`
+  margin-bottom: 10px;
+  border: none;
+  padding: 5px;
+  border-bottom: 1px solid gray;
+`;
+
+const Select = styled.select`
+  margin-bottom: 10px;
+`;
+
+const Option = styled.option``;
+
+const ProductFormRight = styled.div``;
+
+const ProductUpload = styled.div``;
+
+const ProductUploadImg = styled.img``;
+
+const ProductButton = styled.button``;
+
+const Link = styled.link``;
+
+const Chart = styled.div``;
+
+const Publish = styled.button``;
+
+const ProductData = styled.div``;
--- src/Components/adminDashboard/sideProfile/SideProfile.js
+++ src/Components/adminDashboard/sideProfile/SideProfile.js
@@ -1,108 +1,103 @@
-import React, {useState} from 'react'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import styled from "styled-components";
 
 const SideProfile = () => {
+  const [name, setName] = useState("Your Name");
+  const [lastname, setLastname] = useState("Your Lastname");
+  const [email, setEmail] = useState("Your Email");
+  const [username, setUsername] = useState("Your Username");
+  const [password, setPassword] = useState("Your Password");
 
-    const [name,setName] = useState('Your Name');
-    const [lastname,setLastname] = useState('Your Lastname');
-    const [email,setEmail] = useState('Your Email');
-    const [username,setUsername] = useState('Your Username');
-    const [password,setPassword] = useState('Your Password');
+  return (
+    <div>
+      <SidebarContainer className="Card">
+        <SidebarUpperContent className="upper-container">
+          <ProfilePicture className="image-container">
+            <Picture
+              src=""
+              alt=""
+              height="100px"
+              width="100px"
+              border-radius="50%"
+            />
+          </ProfilePicture>
+        </SidebarUpperContent>
+        <SidebarLowerContent className="lower-container">
+          <SidebarInfo> {name} </SidebarInfo>
+          <SidebarInfo> {lastname} </SidebarInfo>
+          <SidebarInfo> {email} </SidebarInfo>
+          <SidebarInfo> {username} </SidebarInfo>
+          <SidebarInfo> {password} </SidebarInfo>
+          <SidebarButton>Visit Profile</SidebarButton>
+          <br />
+          <br />
+          <SidebarButton>Add New Admin </SidebarButton>
+        </SidebarLowerContent>
+      </SidebarContainer>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <SidebarContainer className='Card'>
-                <SidebarUpperContent className="upper-container">
-                    <ProfilePicture className="image-container">
-                        <Picture src='' alt='' height='100px' width='100px' border-radius='50%' />
-                    </ProfilePicture>
-                </SidebarUpperContent>
-                <SidebarLowerContent className='lower-container'>
-                    <SidebarInfo> {name} </SidebarInfo>
-                    <SidebarInfo> {lastname} </SidebarInfo>
-                    <SidebarInfo> {email} </SidebarInfo>
-                    <SidebarInfo> {username} </SidebarInfo>
-                    <SidebarInfo> {password} </SidebarInfo>
-                    <SidebarButton>Visit Profile</SidebarButton>
-                    <br/><br/>
-                    <SidebarButton>Add New Admin </SidebarButton>
-                </SidebarLowerContent>
-            </SidebarContainer>
-            
-        </div>
-    )
-}
-
-export default SideProfile
-
+export default SideProfile;
 
 const SidebarContainer = styled.div`
+  justify-content: space-between;
+  align-items: left;
+  flex-wrap: wrap;
+  position: absolute;
 
-    justify-content: space-between;
-    align-items: left;
-    flex-wrap: wrap;
-    position: absolute;
-
-    color: #000000;
-    border-radius: 83px;
-    margin-top: 150px;
-    width: 300px;
-    height:480px;
-    background: #CDC9C3;
-    overflow: hidden;
+  color: #000000;
+  border-radius: 83px;
+  margin-top: 150px;
+  width: 300px;
+  height: 480px;
+  background: #cdc9c3;
+  overflow: hidden;
 `;
 
 const SidebarUpperContent = styled.div`
-    height: 100px;
-  
-
-
+  height: 100px;
 `;
 
-const ProfilePicture = styled.div`
-
-`;
+const ProfilePicture = styled.div``;
 
 const Picture = styled.img`
-    background: white;
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
-    padding: 5px;
-    transform: translate(95px,45px);
-    border: 2px solid black
+  background: white;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  padding: 5px;
+  transform: translate(95px, 45px);
+  border: 2px solid black;
 `;
 
 const SidebarLowerContent = styled.div`
-    margin-top: 80px;
-    height: 300px;
-    text-align: center;
-    font-family: Raleway;
-    font-size: 0.8rem;
+  margin-top: 80px;
+  height: 300px;
+  text-align: center;
+  font-family: Raleway;
+  font-size: 0.8rem;
 `;
 
-const SidebarInfo = styled.h3`
-
-`;
+const SidebarInfo = styled.h3``;
 
 const SidebarButton = styled.button`
-    font-size: 0.8rem;
-    background: #f774c5;
-    border: none;
-    padding: 0.5rem 1.1rem;
-    color: #fff;
-    border-radius: 1rem;
-    box-shadow: 0px 13px 24px -7px #ecb6d7;
-    transition: all 0.3s ease-in-out;
-    margin-left: 0.5rem;
-    cursor: pointer;
-    &:hover {
-        box-shadow: 0px 17px 16px -11px #ecb6d7;
-        transform: translateY(-5px);
-    }
-    @media (max-width: 670px) {
-        /* width: 100%; */
-        padding: 0.3; 
-    }
-
+  font-size: 0.8rem;
+  background: #f774c5;
+  border: none;
+  padding: 0.5rem 1.1rem;
+  color: #fff;
+  border-radius: 1rem;
+  box-shadow: 0px 13px 24px -7px #ecb6d7;
+  transition: all 0.3s ease-in-out;
+  margin-left: 0.5rem;
+  cursor: pointer;
+  &:hover {
+    box-shadow: 0px 17px 16px -11px #ecb6d7;
+    transform: translateY(-5px);
+  }
+  @media (max-width: 670px) {
+    /* width: 100%; */
+    padding: 0.3;
+  }
 `;
