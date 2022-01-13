f8903a3b9b4534d750da49c31e0be608ac77ce4b
NataliaCK <natalia.chamom@gmail.com>
2022-01-13 23:02:38 +1000

    took out the mongoDB problem

--- src/Components/adminDashboard/Form/indexTwo.js
+++ src/Components/adminDashboard/Form/indexTwo.js
@@ -1,40 +1,40 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const { default: ListFilm } = require("../ListFilm/ListFilm");
-const app = express();
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const { default: ListFilm } = require("../ListFilm/ListFilm");
+// const app = express();
 
-const ListFilmModel = require("./models/filmModel ");
+// const ListFilmModel = require("./models/filmModel ");
 
-app.use(express.json());
+// app.use(express.json());
 
-mongoose.connect(
-  "mongodb+srv://ablackwood:kH6DEywNVpVXsmct@cluster0.vgef1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-  }
-);
+// mongoose.connect(
+//   "mongodb+srv://ablackwood:kH6DEywNVpVXsmct@cluster0.vgef1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+//   {
+//     useNewUrlParser: true,
+//   }
+// );
 
-app.get("/", async (req, res) => {
-  const filmName = req.body.filmName;
-  const year = req.body.year;
-  const duration = req.body.duration;
-  const price = req.body.price;
-  const summary = req.body.summary;
-  const description = req.body.description;
+// app.get("/", async (req, res) => {
+//   const filmName = req.body.filmName;
+//   const year = req.body.year;
+//   const duration = req.body.duration;
+//   const price = req.body.price;
+//   const summary = req.body.summary;
+//   const description = req.body.description;
 
-  const film = new ListFilmModel({
-    filmName: filmName,
-    year: year,
-    duration: duration,
-    price: price,
-    summary: summary,
-    description: description,
-  });
+//   const film = new ListFilmModel({
+//     filmName: filmName,
+//     year: year,
+//     duration: duration,
+//     price: price,
+//     summary: summary,
+//     description: description,
+//   });
 
-  try {
-    await film.save();
-    res.send("inserted data");
-  } catch (err) {
-    console.log(err);
-  }
-});
+//   try {
+//     await film.save();
+//     res.send("inserted data");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
